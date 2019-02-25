e2edd14306493422920695a1ed3a5bdb55fe5a8e
Wenhui Fang <46543296+windy030@users.noreply.github.com>
2019-02-25 14:51:23 -0500

    Validation Created

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -7,7 +7,35 @@ load_dotenv() # loads environment variables set in a ".env" file, including the 
 
 # see: https://www.alphavantage.co/support/#api-key
 api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
-print("API KEY: " + api_key) # TODO: remove or comment-out this line after you have verified the environment variable is getting read properly
+# print("API KEY: " + api_key) # TODO: remove or comment-out this line after you have verified the environment variable is getting read properly
+
+
+# Modifying my previous data validation to work for this project
+while True:
+  datatype_pass = True
+  symbol_length_pass = True
+  symbol_pass = True
+  program_pass = True
+  stock_symbol = input("Please enter the stock symbol you would like to hear advice for...")
+
+  #Input validation
+  if not stock_symbol.isalpha():
+    print("INPUT DATA TYPE ERROR! Please only enter three to five characters! Let's try again.")
+    datatype_pass = False
+  if datatype_pass == True:
+    # New York Stock Exchange (NYSE) and American Stock Exchange (AMEX) listed stocks have three characters or less. 
+    # Nasdaq-listed securities have four or five characters.
+    if int(len(stock_symbol)) not in range(3,5):
+        print("Please ensure the length of the symbol is between three and five. Let's try again.")
+        symbol_length_pass = False
+    if symbol_length_pass ==True:    
+        #validation adapted from https://github.com/hiepnguyen034/robo-stock/blob/master/robo_advisor.py
+        data=requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+stock_symbol+'&apikey='+api_key)
+        if 'Error' in data.text:
+            print('The stock you are looking for is not here')
+            break
+        else:
+            break
 
 symbol = "NFLX" # TODO: capture user input, like... input("Please specify a stock symbol: ")
 
