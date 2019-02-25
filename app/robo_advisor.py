56771d13cc1fee9d4f71a8b80fa3f045450faa78
Wenhui Fang <46543296+windy030@users.noreply.github.com>
2019-02-25 15:15:44 -0500

    Update robo_advisor.py

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -5,11 +5,9 @@ import requests
 
 load_dotenv() # loads environment variables set in a ".env" file, including the value of the ALPHAVANTAGE_API_KEY variable
 
-
 # see: https://www.alphavantage.co/support/#api-key
 api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
 # print("API KEY: " + api_key) # TODO: remove or comment-out this line after you have verified the environment variable is getting read properly
-
 
 # Modifying my previous data validation to work for this project
 while True:
@@ -32,49 +30,54 @@ while True:
         #validation adapted from https://github.com/hiepnguyen034/robo-stock/blob/master/robo_advisor.py
         data=requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+stock_symbol+'&apikey='+api_key)
         if 'Error' in data.text:
-            print('The stock you are looking for is not here')
+            print("Sorry.The stock symbol is either incorrect or cannot be found online. The program will terminate now...")
             break
         else:
             program_pass = True
             break
 
-print(program_pass)
-print(data)
+if program_pass == True:
 
-request_url = ('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+stock_symbol+'&apikey='+api_key)
-response = requests.get(request_url)
-print(type(response.status_code))
-print(response.text)
+  print(program_pass)
+  print(data)
 
+  request_url = ('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+stock_symbol+'&apikey='+api_key)
+  response = requests.get(request_url)
+  print(type(response.status_code))
+  # print(response.text)
 
-symbol = "NFLX" # TODO: capture user input, like... input("Please specify a stock symbol: ")
+  parsed_response = json.loads(response.text)
 
-# see: https://www.alphavantage.co/documentation/#daily (or a different endpoint, as desired)
-# TODO: assemble the request url to get daily data for the given stock symbol...
+  print(type(parsed_response))
 
-# TODO: use the "requests" package to issue a "GET" request to the specified url, and store the JSON response in a variable...
+  symbol = "NFLX" # TODO: capture user input, like... input("Please specify a stock symbol: ")
 
-# TODO: further parse the JSON response...
+  # see: https://www.alphavantage.co/documentation/#daily (or a different endpoint, as desired)
+  # TODO: assemble the request url to get daily data for the given stock symbol...
 
-# TODO: traverse the nested response data structure to find the latest closing price and other values of interest...
-latest_price_usd = "$100,000.00"
+  # TODO: use the "requests" package to issue a "GET" request to the specified url, and store the JSON response in a variable...
 
-#
-# INFO OUTPUTS
-#
+  # TODO: further parse the JSON response...
 
-# TODO: write response data to a CSV file
+  # TODO: traverse the nested response data structure to find the latest closing price and other values of interest...
+  latest_price_usd = "$100,000.00"
 
-# TODO: further revise the example outputs below to reflect real information
-print("-----------------")
-print(f"STOCK SYMBOL: {symbol}")
-print("RUN AT: 11:52pm on June 5th, 2018")
-print("-----------------")
-print("LATEST DAY OF AVAILABLE DATA: June 4th, 2018")
-print(f"LATEST DAILY CLOSING PRICE: {latest_price_usd}")
-print("RECENT AVERAGE HIGH CLOSING PRICE: $101,000.00")
-print("RECENT AVERAGE LOW CLOSING PRICE: $99,000.00")
-print("-----------------")
-print("RECOMMENDATION: Buy!")
-print("RECOMMENDATION REASON: Because the latest closing price is within threshold XYZ etc., etc. and this fits within your risk tolerance etc., etc.")
-print("-----------------")
+  #
+  # INFO OUTPUTS
+  #
+
+  # TODO: write response data to a CSV file
+
+  # TODO: further revise the example outputs below to reflect real information
+  print("-----------------")
+  print(f"STOCK SYMBOL: {symbol}")
+  print("RUN AT: 11:52pm on June 5th, 2018")
+  print("-----------------")
+  print("LATEST DAY OF AVAILABLE DATA: June 4th, 2018")
+  print(f"LATEST DAILY CLOSING PRICE: {latest_price_usd}")
+  print("RECENT AVERAGE HIGH CLOSING PRICE: $101,000.00")
+  print("RECENT AVERAGE LOW CLOSING PRICE: $99,000.00")
+  print("-----------------")
+  print("RECOMMENDATION: Buy!")
+  print("RECOMMENDATION REASON: Because the latest closing price is within threshold XYZ etc., etc. and this fits within your risk tolerance etc., etc.")
+  print("-----------------")
